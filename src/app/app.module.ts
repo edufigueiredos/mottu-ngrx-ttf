1e7642d517112a75479730d43f8febb616842c45
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-06-30 23:02:48 -0300

    Adicionando get name no app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetNameModule } from './modules/get-name/get-name.module';
 import { InputNameModule } from './modules/input-name/input-name.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, InputNameModule],
+  imports: [BrowserModule, AppRoutingModule, InputNameModule, GetNameModule],
   providers: [],
   bootstrap: [AppComponent],
 })
