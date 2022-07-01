e9156bc1b936848a0bdb2211d152945e564aaaf9
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-07-01 00:16:39 -0300

    Removendo nomes da lista

--- src/app/modules/list-name/list-name.component.ts
+++ src/app/modules/list-name/list-name.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getNames } from 'src/app/shared/+ state/name/name.selectors';
+import { deleteName } from 'src/app/shared/+ state/name/name.actions';
 
 @Component({
   selector: 'app-list-name',
   templateUrl: './list-name.component.html',
-  styleUrls: ['./list-name.component.scss']
+  styleUrls: ['./list-name.component.scss'],
 })
 export class ListNameComponent {
-
   nameList$ = this.store.select(getNames);
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
+  removeName(name: string) {
+    this.store.dispatch(deleteName({ name }));
+  }
 }
